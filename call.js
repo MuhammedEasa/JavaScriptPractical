a2c02cda86ce5927e743d51e258ca5edf9e8b842
Muhammed Easa <easanedumangad@gmail.com>
2024-03-21 10:25:04 +0530

    Call Apply,Bind Code

--- call.js
+++ call.js
@@ -1,12 +1,36 @@
-function getName(){
-    console.log(this.name);
-    console.log(this.age);
+//call,apply, bind
+
+function getName(a, b, c) {
+  console.log(a, b, c);
+  console.log(this.name);
+  console.log(this.age);
 }
 
+const person1 = {
+  name: "Easa",
+  age: 19,
+};
 
-const person1 ={
-    name:"Easa",
-    age:19
+getName.call(person1, "1", "2", "3");
+getName.apply(person1, ["1", "2", "3"]);
+const neww = getName.bind(person1, "1", "2", "3");
+neww();
+
+class Hai {
+  constructor() {
+    this.name = "Hello";
+  }
+  getName() {
+    return this.name;
+  }
 }
 
-getName.call(person1)
+const user = new Hai();
+console.log(user.getName()); //Hello
+
+//! this is a error
+//const value = user.getName;
+//console.log(value());// undefined
+
+const value2 = user.getName.bind(user);
+console.log(value2()); // Hello
